654b13b0fe23a71ce9c2e4219e0ca9ea2890925e
Malthe <malthe@Malthes-MacBook-Pro.local>
2018-11-26 16:07:19 +0100

    opdateret intellij

--- src/main/java/com/cbsexam/UserEndpoints.java
+++ src/main/java/com/cbsexam/UserEndpoints.java
@@ -18,27 +18,27 @@ import utils.Log;
 public class UserEndpoints {
 
 
+
     public static UserCache userCache = new UserCache();
     public static Hashing hashing = new Hashing();
 
-
-    /**
-     * @param token
-     * @return Responses
-     */
-    @GET
-    @Path("/byID/{token}")
-    public Response getUser(@PathParam("token") String token){
+        /**
+         * @param token
+         * @return Responses
+         */
+        @GET
+        @Path("/byID/{token}")
+        public Response getUser (@PathParam("token") String token){
 
 
         // TODO: What should happen if something breaks down? : fixed
-        try{
+        try {
             String json;
 
             ArrayList<User> users = userCache.getUsers(false);
 
-            for(User user : users){
-                if(user.getToken()!= null && user.getToken().equals(token)){
+            for (User user : users) {
+                if (user.getToken() != null && user.getToken().equals(token)) {
 
                     json = new Gson().toJson(user);
                     // Return the user with the status code 200
@@ -53,7 +53,7 @@ public class UserEndpoints {
             json = Encryption.encryptDecryptXOR(json);
             return Response.status(400).entity("Session ID not valid\n" + json).build();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return Response.status(400).entity("Something went wrong").build();
         }
@@ -61,142 +61,142 @@ public class UserEndpoints {
     }
 
 
-    /** @return Responses */
-  @GET
-  @Path("/{token}")
-  public Response getUsers(@PathParam("token") String token) {
+        /** @return Responses */
+        @GET
+        @Path("/{token}")
+        public Response getUsers (@PathParam("token") String token){
 
-    //Malthe: Get a list of users from the cache function
-    ArrayList<User> users = userCache.getUsers(false);
-    ArrayList<User> usersWithoutToken = new ArrayList<>();
+        //Malthe: Get a list of users from the cache function
+        ArrayList<User> users = userCache.getUsers(false);
+        ArrayList<User> usersWithoutToken = new ArrayList<>();
 
-      boolean check = true;
+        boolean check = true;
 
-      //Malthe: Checks if the granted token is valid in DB
-    for(User user: users){
-        if(user.getToken() !=null && user.getToken().equals(token)){
+        //Malthe: Checks if the granted token is valid in DB
+        for (User user : users) {
+            if (user.getToken() != null && user.getToken().equals(token)) {
 
-            //Malthe: sets the check to false so the json String object is not encrypted if a valid token is granted
-            check = false;
+                //Malthe: sets the check to false so the json String object is not encrypted if a valid token is granted
+                check = false;
 
-            // Write to log that we are here
-            Log.writeLog(this.getClass().getName(), this, "Get all users", 0);
+                // Write to log that we are here
+                Log.writeLog(this.getClass().getName(), this, "Get all users", 0);
+            }
+            //Malthe: Create new User object without password and token
+            User user1 = new User(0, user.getFirstname(), user.getLastname(), null, user.getEmail(), user.getCreatedTime(), user.getUsername(), null);
+
+            //Malthe: Add to the arraylist that shoukd be printed
+            usersWithoutToken.add(user1);
         }
-        //Malthe: Create new User object without password and token
-        User user1 = new User(0, user.getFirstname(), user.getLastname(),null, user.getEmail(), user.getCreatedTime(), user.getUsername(), null);
+        // Transfer users to json in order to return it to the user
+        String json = new Gson().toJson(usersWithoutToken);
 
-        //Malthe: Add to the arraylist that shoukd be printed
-        usersWithoutToken.add(user1);
-    }
-      // Transfer users to json in order to return it to the user
-      String json = new Gson().toJson(usersWithoutToken);
+        // TODO: Add Encryption to JSON : fixed
+        //Malthe: Add encryption to json rawString object(ref. utils Encryption) if no user is found
+        if (check) {
+            json = Encryption.encryptDecryptXOR(json);
+            // Write to log that we are here
+            Log.writeLog(this.getClass().getName(), this, "Get all users encrypted", 0);
+        }
 
-      // TODO: Add Encryption to JSON : fixed
-      //Malthe: Add encryption to json rawString object(ref. utils Encryption) if no user is found
-      if (check) {
-        json = Encryption.encryptDecryptXOR(json);
-          // Write to log that we are here
-          Log.writeLog(this.getClass().getName(), this, "Get all users encrypted", 0);
+        // Return the users with the status code 200
+        return Response.status(200).type(MediaType.APPLICATION_JSON).entity(json).build();
     }
 
-    // Return the users with the status code 200
-      return Response.status(200).type(MediaType.APPLICATION_JSON).entity(json).build();
-  }
+        @POST
+        @Path("/")
+        @Consumes(MediaType.APPLICATION_JSON)
+        public Response createUser (String body){
 
-  @POST
-  @Path("/")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Response createUser(String body) {
+        try {
+            // Read the json from body and transfer it to a user class
+            User newUser = new Gson().fromJson(body, User.class);
 
-    try{
-        // Read the json from body and transfer it to a user class
-        User newUser = new Gson().fromJson(body, User.class);
+            // Use the controller to add the user
+            User createUser = UserController.createUser(newUser);
 
-        // Use the controller to add the user
-        User createUser = UserController.createUser(newUser);
+            //Malthe: Force update of the user cache, since there is a new user in the ArrayList of users
+            userCache.getUsers(true);
 
-        //Malthe: Force update of the user cache, since there is a new user in the ArrayList of users
+            // Get the user back with the added ID and return it to the user
+            String json = new Gson().toJson(createUser);
+
+            // Checks if the username is already taken
+            if (createUser != null) {
+                // Return a response with status 200 and JSON as type
+                return Response.status(200).type(MediaType.APPLICATION_JSON_TYPE).entity(json).build();
+            } else {
+                return Response.status(400).entity("Could not create user - username might have been taken").build();
+            }
+        } catch (Exception e) {
+            return Response.status(400).entity("Username already taken").build();
+        }
+
+    }
+
+        // TODO: Make a smart way of login in without having to enter ID, maybe not possible : fixed (implemented username)
+        // TODO: Make the system able to login users and assign them a token to use throughout the system : fixed
+        @POST
+        @Path("/login")
+        @Consumes(MediaType.APPLICATION_JSON)
+        public Response loginUser (String login){
+
+        User userLogin = new Gson().fromJson(login, User.class);
+
+        String token = UserController.auth(userLogin);
+
+        //Malthe: Cache update since token will be updated
         userCache.getUsers(true);
 
-        // Get the user back with the added ID and return it to the user
-        String json = new Gson().toJson(createUser);
-
-        // Checks if the username is already taken
-        if (createUser != null) {
-            // Return a response with status 200 and JSON as type
-            return Response.status(200).type(MediaType.APPLICATION_JSON_TYPE).entity(json).build();
-        } else {
-            return Response.status(400).entity("Could not create user - username might have been taken").build();
+        if (token != null) {
+            return Response.status(200).entity("Your token is:\n" + token).build();
         }
-    } catch (Exception e){
-        return Response.status(400).entity("Username already taken").build();
+
+        // Return a response with status 200 and JSON as type
+        return Response.status(400).entity("Not valid login attempt. Please match your input").build();
     }
 
-  }
+        // TODO: Make the system able to delete users : fixed
+        @POST
+        @Path("/delete/{token}")
+        @Consumes(MediaType.APPLICATION_JSON)
+        public Response deleteUser (@PathParam("token") String token){
 
-  // TODO: Make a smart way of login in without having to enter ID, maybe not possible : fixed (implemented username)
-  // TODO: Make the system able to login users and assign them a token to use throughout the system : fixed
-  @POST
-  @Path("/login")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Response loginUser(String login) {
 
-    User userLogin = new Gson().fromJson(login, User.class);
+        ArrayList<User> users = userCache.getUsers(false);
 
-    String token = UserController.auth(userLogin);
+        //Malthe: Autherise the users token
+        for (User user : users) {
+            if (user.getToken() != null && user.getToken().equals(token)) {
 
-    //Malthe: Cache update since token will be updated
-    userCache.getUsers(true);
+                //Malthe: Calls method in usercontroller that deletes found user
+                UserController.deleteUser(user.getId());
 
-    if(token != null){
-        return Response.status(200).entity("Your token is:\n" + token).build();
+                //Malthe: Update of the user cache, since there is deleted a User in the ArrayList of users
+                userCache.getUsers(true);
+
+                // Return a response with status 200 and JSON as type
+                return Response.status(200).entity("User with id " + user.getId() + " is now deleted").build();
+            }
+
+        }
+
+        return Response.status(400).entity("Your session ID is not valid").build();
     }
 
-    // Return a response with status 200 and JSON as type
-    return Response.status(400).entity("Not valid login attempt. Please match your input").build();
-  }
-
-  // TODO: Make the system able to delete users : fixed
-  @POST
-  @Path("/delete/{token}")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Response deleteUser(@PathParam("token") String token) {
-
-
-          ArrayList<User> users = userCache.getUsers(false);
-
-          //Malthe: Autherise the users token
-          for(User user: users){
-              if(user.getToken() !=null && user.getToken().equals(token)){
-
-                  //Malthe: Calls method in usercontroller that deletes found user
-                  UserController.deleteUser(user.getId());
-
-                  //Malthe: Update of the user cache, since there is deleted a User in the ArrayList of users
-                  userCache.getUsers(true);
-
-                  // Return a response with status 200 and JSON as type
-                  return Response.status(200).entity("User with id " + user.getId() + " is now deleted").build();
-              }
-
-          }
-
-      return Response.status(400).entity("Your session ID is not valid").build();
-  }
-
-  // TODO: Make the system able to update users : fixed
-  @POST
-  @Path("/update/{token}")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Response updateUser(String updatedUserData, @PathParam("token") String token) {
+        // TODO: Make the system able to update users : fixed
+        @POST
+        @Path("/update/{token}")
+        @Consumes(MediaType.APPLICATION_JSON)
+        public Response updateUser (String updatedUserData, @PathParam("token") String token){
 
 
         ArrayList<User> users = userCache.getUsers(false);
 
         User updatedUserDataObj = new Gson().fromJson(updatedUserData, User.class);
 
-        for(User user:users){
-            if(user.getToken() != null && user.getToken().equals(token)){
+        for (User user : users) {
+            if (user.getToken() != null && user.getToken().equals(token)) {
 
                 //Malthe: Calls method in usercontroller that updates with new info
                 UserController.updateUser(user, updatedUserDataObj);
@@ -213,71 +213,71 @@ public class UserEndpoints {
             }
         }
 
-      return Response.status(400).entity("Your session ID is not valid").build();
-  }
+        return Response.status(400).entity("Your session ID is not valid").build();
+    }
 
-  @POST
-  @Path("/updatepassword/{token}")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Response updatePassword(String passwordUpdate, @PathParam("token") String token){
+        @POST
+        @Path("/updatepassword/{token}")
+        @Consumes(MediaType.APPLICATION_JSON)
+        public Response updatePassword (String passwordUpdate, @PathParam("token") String token){
 
-          //Malthe: The inputtet password is saved in a user object and the password is saved in a string
-          User passwordupdate = new Gson().fromJson(passwordUpdate, User.class);
-          String password = passwordupdate.getPassword();
+        //Malthe: The inputtet password is saved in a user object and the password is saved in a string
+        User passwordupdate = new Gson().fromJson(passwordUpdate, User.class);
+        String password = passwordupdate.getPassword();
 
-          ArrayList<User> users = userCache.getUsers(false);
+        ArrayList<User> users = userCache.getUsers(false);
 
-          for(User user: users){
-              if(user.getToken()!= null && user.getToken().equals(token)){
-                  //Malthe: Getting the user which password needs to be updated to currentuser
-                  User usertoUpdate = UserController.getUser(user.getId());
+        for (User user : users) {
+            if (user.getToken() != null && user.getToken().equals(token)) {
+                //Malthe: Getting the user which password needs to be updated to currentuser
+                User usertoUpdate = UserController.getUser(user.getId());
 
-                  //Malthe: Setting the salt to currentusers created time
-                  hashing.setSalt(String.valueOf(usertoUpdate.getCreatedTime()));
+                //Malthe: Setting the salt to currentusers created time
+                hashing.setSalt(String.valueOf(usertoUpdate.getCreatedTime()));
 
-                  //Malthe: Hashing the new password with createdtime as the salt
-                  String newPassword = hashing.hashWithSaltSHA(password);
+                //Malthe: Hashing the new password with createdtime as the salt
+                String newPassword = hashing.hashWithSaltSHA(password);
 
-                  //Malthe: If the new password is not "new" or too short
-                  if(usertoUpdate.getPassword().equals(newPassword) || password.length() < 7){
-                      return Response.status(400).entity("Password must be new and 6 characters long").build();
-                  }else{
-                      //Malthe: Updating
-                      UserController.updatePassword(user.getId(), newPassword);
+                //Malthe: If the new password is not "new" or too short
+                if (usertoUpdate.getPassword().equals(newPassword) || password.length() < 7) {
+                    return Response.status(400).entity("Password must be new and 6 characters long").build();
+                } else {
+                    //Malthe: Updating
+                    UserController.updatePassword(user.getId(), newPassword);
 
-                      //Malthe: Forcing a update in the cache since the list of users has changed
-                      userCache.getUsers(true);
+                    //Malthe: Forcing a update in the cache since the list of users has changed
+                    userCache.getUsers(true);
 
-                      return Response.status(200).entity("Password is updated and hashed").build();
-                  }
-              }
-          }
+                    return Response.status(200).entity("Password is updated and hashed").build();
+                }
+            }
+        }
 
-      return Response.status(400).entity("Session ID not valid").build();
+        return Response.status(400).entity("Session ID not valid").build();
 
-  }
+    }
 
-  @GET
-  @Path("/logout/{token}")
-  public Response logout(@PathParam("token")String token){
+        @GET
+        @Path("/logout/{token}")
+        public Response logout (@PathParam("token") String token){
 
-      ArrayList<User> users = userCache.getUsers(false);
+        ArrayList<User> users = userCache.getUsers(false);
 
-      for(User user : users){
-          if(user.getToken() != null && user.getToken().equals(token)){
+        for (User user : users) {
+            if (user.getToken() != null && user.getToken().equals(token)) {
 
-              UserController.logout(user);
+                UserController.logout(user);
 
-              //Malthe: Cache update since token will be updated
-              userCache.getUsers(true);
+                //Malthe: Cache update since token will be updated
+                userCache.getUsers(true);
 
-              return Response.status(200).entity("You are now logged out").build();
+                return Response.status(200).entity("You are now logged out").build();
 
-          }
-      }
+            }
+        }
 
-      // Return a response with status 200 and JSON as type
-      return Response.status(400).entity("Session ID not valid").build();
-  }
+        // Return a response with status 200 and JSON as type
+        return Response.status(400).entity("Session ID not valid").build();
+    }
 
 }
